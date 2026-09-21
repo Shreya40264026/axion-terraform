692ac9842538fa9cb42297fe60912701aad2c8c6
unknown <shreya.devops211@gmail.com>
2026-09-22 00:48:03 +0530

    second commit

--- provider.tf
+++ provider.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "value"
-    storage_account_name = "value"
-    container_name       = "value"
-    key                  = "value"
+    resource_group_name  = "shreya-axion-rg"
+    storage_account_name = "shreyaaxionstorage"
+    container_name       = "axion-container"
+    key                  = "axion-terraform.tfvars"
   }
 }
 
